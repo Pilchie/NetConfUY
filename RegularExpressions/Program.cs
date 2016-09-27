1a72fef8a5031a8002110264fc560773bc7fc743
Kevin Pilch-Bisson <kevinpi@microsoft.com>
2016-09-27 14:35:30 -0700

    Add  Regular Expression analyzer

---
+++ RegularExpressions/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Regex("[abcSomething");
+        }
+    }
+}
