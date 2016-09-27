8132888cf5d7211d29845eca4c6b527ec50c4f76
Kevin Pilch-Bisson <kevinpi@microsoft.com>
2016-09-27 13:00:21 -0700

    Get language demos working

--- FutureOfCSharp/LanguageFeatures.cs
+++ FutureOfCSharp/LanguageFeatures.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
- 
+
 using static System.Console;
- 
+
 class Program
 {
     static void Main(string[] args)
@@ -13,17 +13,17 @@ class Program
         object[] numbers =
             { 0b1, 0b10, new object[] { 0b100, 0b1000 },   // binary literals
              0b1_0000, 0b10_0000 };                        // digit separators
- 
+
         var (sum, count) = Tally(numbers);                 // deconstruction
         WriteLine($"Sum: {sum}, Count: {count}");
     }
- 
+
     static (int sum, int count) Tally(object[] values)     // tuple types
     {
         var r = (s: 0, c: 0);                              // tuple literals
- 
+
         void Add(int s, int c) { r = (r.s + s, r.c + c); } // local functions
- 
+
         foreach (var v in values)
         {
             switch (v)                                     // switch on any value
