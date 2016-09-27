4be4c3032bcfacf47e98539428dcfa14dcbc0f67
Kevin Pilch-Bisson <kevinpi@microsoft.com>
2016-09-27 15:37:07 -0700

    Implement fixer

--- UseNameof/UseNameof/CodeFixProvider.cs
+++ UseNameof/UseNameof/CodeFixProvider.cs
@@ -12,13 +12,14 @@ using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Rename;
 using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis.Editing;
 
 namespace UseNameof
 {
     [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(UseNameofCodeFixProvider)), Shared]
     public class UseNameofCodeFixProvider : CodeFixProvider
     {
-        private const string _title = "Make uppercase";
+        private const string _title = "Replace with nameof";
 
         public sealed override ImmutableArray<string> FixableDiagnosticIds
         {
@@ -35,39 +36,32 @@ namespace UseNameof
         {
             var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
 
-            // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
             var diagnostic = context.Diagnostics.First();
             var diagnosticSpan = diagnostic.Location.SourceSpan;
 
-            // Find the type declaration identified by the diagnostic.
-            var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<TypeDeclarationSyntax>().First();
+            var literal = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<LiteralExpressionSyntax>().First();
 
             // Register a code action that will invoke the fix.
             context.RegisterCodeFix(
                 CodeAction.Create(
                     title: _title,
-                    createChangedSolution: c => MakeUppercaseAsync(context.Document, declaration, c),
+                    createChangedDocument: c => ReplaceLiteralWithNameofAsync(context.Document, literal, c),
                     equivalenceKey: _title),
                 diagnostic);
         }
 
-        private async Task<Solution> MakeUppercaseAsync(Document document, TypeDeclarationSyntax typeDecl, CancellationToken cancellationToken)
+        private async Task<Document> ReplaceLiteralWithNameofAsync(
+            Document document,
+            LiteralExpressionSyntax literal,
+            CancellationToken cancellationToken)
         {
-            // Compute new uppercase name.
-            var identifierToken = typeDecl.Identifier;
-            var newName = identifierToken.Text.ToUpperInvariant();
-
-            // Get the symbol representing the type to be renamed.
-            var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
-            var typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
-
-            // Produce a new solution that has all references to that type renamed, including the declaration.
-            var originalSolution = document.Project.Solution;
-            var optionSet = originalSolution.Workspace.Options;
-            var newSolution = await Renamer.RenameSymbolAsync(document.Project.Solution, typeSymbol, newName, optionSet, cancellationToken).ConfigureAwait(false);
-
-            // Return the new solution with the now-uppercase type name.
-            return newSolution;
+            var editor = await DocumentEditor.CreateAsync(document, cancellationToken);
+            editor.ReplaceNode(
+                literal,
+                SyntaxFactory.InvocationExpression(
+                    SyntaxFactory.ParseExpression("nameof")).AddArgumentListArguments(
+                        SyntaxFactory.Argument(SyntaxFactory.ParseExpression(literal.Token.ValueText))));
+            return editor.GetChangedDocument();
         }
     }
 }
